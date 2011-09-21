25342b573e1c772eee6ebf1c81da16cffa464f25
Richard Cruz <cruzitorc@gmail.com>
2011-09-21 16:41:27 -0430

    sorcery

---
+++ test/functional/home_controller_test.rb
@@ -0,0 +1,14 @@
+require 'test_helper'
+
+class HomeControllerTest < ActionController::TestCase
+  test "should get index" do
+    get :index
+    assert_response :success
+  end
+
+  test "should get secret" do
+    get :secret
+    assert_response :success
+  end
+
+end
